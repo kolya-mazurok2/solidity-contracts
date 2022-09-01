0f314f45d12d2b0783afa74f82744003388d710f
kolya.mazurok2 <kolya.mazurok2@gmail.com>
2022-09-01 17:00:49 +0300

    Fixed increment, added rollback functions to erc20 and erc721

--- contracts/EscrowERC20.sol
+++ contracts/EscrowERC20.sol
@@ -3,11 +3,23 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract EscrowERC20 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _transactionCounter;
+
     address internal owner;
     mapping(bytes32 => address) public allowedTokens;
-    mapping(address => mapping (bytes32 => uint256)) public balances;
+    mapping(address => mapping(bytes32 => uint256)) public balances;
+    mapping(uint256 => Transcation) private transactions;
+
+    struct Transcation {
+        address sender;
+        address receiver;
+        bytes32 symbol;
+        uint256 amount;
+    }
 
     constructor() {
         owner = msg.sender;
@@ -19,10 +31,28 @@ contract EscrowERC20 {
         allowedTokens[_symbol] = _tokenAddress;
     }
 
-
-    function deposit(uint256 _amount, bytes32 _symbol, address _receiver) external  {
+    function deposit(
+        uint256 _amount,
+        bytes32 _symbol,
+        address _receiver
+    ) external returns (uint256) {
         balances[_receiver][_symbol] += _amount;
-        ERC20(allowedTokens[_symbol]).transferFrom(msg.sender, address(this), _amount);
+        ERC20(allowedTokens[_symbol]).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+        uint256 transactionId = _transactionCounter.current();
+        transactions[transactionId] = Transcation({
+            sender: msg.sender,
+            receiver: _receiver,
+            symbol: _symbol,
+            amount: _amount
+        });
+
+        _transactionCounter.increment();
+
+        return transactionId;
     }
 
     function withdraw(uint256 _amount, bytes32 _symbol) external {
@@ -30,5 +60,23 @@ contract EscrowERC20 {
 
         balances[msg.sender][_symbol] -= _amount;
         ERC20(allowedTokens[_symbol]).transfer(msg.sender, _amount);
+    }
+
+    function rollback(uint256 transactionId) public {
+        address receiver = transactions[transactionId].receiver;
+        uint256 transactionAmount = transactions[transactionId].amount;
+        bytes32 symbol = transactions[transactionId].symbol;
+
+        require(
+            transactions[transactionId].sender == msg.sender,
+            "Not a sender"
+        );
+        require(
+            balances[receiver][symbol] >= transactionAmount,
+            "Insufficient funds"
+        );
+
+        balances[receiver][symbol] -= transactionAmount;
+        ERC20(allowedTokens[symbol]).transfer(msg.sender, transactionAmount);
     }
 }
--- contracts/EscrowERC721.sol
+++ contracts/EscrowERC721.sol
@@ -23,10 +23,11 @@ contract EscrowERC721 {
         token = _token;
     }
 
-    function deposit(address _receiver, uint256 _itemId) public {
+    function deposit(address _receiver, uint256 _itemId)
+        external
+        returns (uint256)
+    {
         require(msg.sender == token.ownerOf(_itemId), "Not an owner");
-
-        _tokenIds.increment();
 
         uint256 id = _tokenIds.current();
         token.transferFrom(msg.sender, address(this), _itemId);
@@ -35,12 +36,26 @@ contract EscrowERC721 {
             receiver: _receiver,
             itemId: _itemId
         });
+
+        _tokenIds.increment();
+
+        return id;
     }
 
     function withdraw(uint256 _id) public {
         address receiver = items[_id].receiver;
         uint256 itemId = items[_id].itemId;
-        delete(items[_id]);
+        delete (items[_id]);
         token.transferFrom(address(this), receiver, itemId);
+    }
+
+    function rollback(uint256 transactionId) public {
+        address sender = items[transactionId].sender;
+        uint256 itemId = items[transactionId].itemId;
+
+        require(sender == msg.sender, "Not a sender");
+
+        delete (items[itemId]);
+        token.transferFrom(address(this), sender, itemId);
     }
 }
